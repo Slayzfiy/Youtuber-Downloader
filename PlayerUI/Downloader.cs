79ea67d42182da5dbaeb01309451386bf49d5e53
Slayzfiy <45815404+Slayzfiy@users.noreply.github.com>
2020-06-23 13:43:16 +0200

    downloader

--- PlayerUI/Downloader.cs
+++ PlayerUI/Downloader.cs
@@ -6,11 +6,12 @@ using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Text;
 
 namespace PlayerUI
 {
-
-
     public class Downloader
     {
         private string api_token = "AIzaSyB1EhfuAjpxzXsTT6koEC_N9lfMhMvkW0U";
@@ -21,7 +22,7 @@ namespace PlayerUI
             File.WriteAllBytes(@"D:\" + video.FullName, video.GetBytes());
         }
 
-        public string Save_Video_KEYWORD(string keyword)
+        public List<VideoObject> Search_Video(string keyword)
         {
             string link = $"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q={keyword}&type=video&key={api_token}";
             using (var wb = new WebClient())
@@ -32,27 +33,39 @@ namespace PlayerUI
                 var temp_video_id = tmpResult["items"].Values("id").Values("videoId").ToArray();
                 var temp_video_thumbnail = tmpResult["items"].Values("snippet").Values("thumbnails").Values("high").Values("url").ToArray();
 
-                return temp_video_title[0].ToString();
-                foreach (var item in temp_video_id)
+                List<VideoObject> video_list = new List<VideoObject>();
+                for (int i = 0; i < 4; i++) 
                 {
-                    string video_url = $"https://www.youtube.com/watch?v={item}";
-                    //VideoObject o = new VideoObject("hallo", video_url, temp_video_thumbnail);
+                    string temp_video_url = string.Format("https://www.youtube.com/watch?v={0}", temp_video_id[i].ToString());
+                    VideoObject video_obj = new VideoObject(temp_video_title[i].ToString(), temp_video_url, temp_video_thumbnail[i].ToString());
+                    video_list.Add(video_obj);
                 }
 
-                foreach (var item in temp_video_thumbnail)
-                {
-
-                }
-               
-                
-               
-
-                // finished 
-                //string id = video_id[0].ToString();
-                //string youtube_url = $"https://www.youtube.com/watch?v={id}";
-                //Save_Video_LINK(youtube_url);
-                //return youtube_url;
+                return video_list; 
             }
+        }
+
+        public async Task<Bitmap> GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+            try
+            {
+                using (var webClient = new WebClient())
+                {
+                    var imageBytes = await webClient.DownloadDataTaskAsync(url);
+                    if (imageBytes != null && imageBytes.Length > 0)
+                    {
+                        //imageBitmap = BitmapFactory.DecodeByteArray(Encoding.ASCII.GetBytes(imageBytes), 0, imageBytes.Length);
+                        imageBitmap = BitmapFactory.DecodeByteArray(Encoding.BigEndianUnicode.GetBytes(imageBytes), 0, imageBytes.Length);
+                    }
+                }
+            }
+            catch
+            {
+                //Silence is gold.
+            }
+           
+            return imageBitmap;
         }
     }
 }
