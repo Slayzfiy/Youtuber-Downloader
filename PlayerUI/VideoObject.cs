a7c0ca58adb1837c1d7d7f8fab79aa6cb769974c
Slayzfiy <45815404+Slayzfiy@users.noreply.github.com>
2020-06-23 14:14:54 +0200

    ds

--- PlayerUI/VideoObject.cs
+++ PlayerUI/VideoObject.cs
@@ -2,7 +2,7 @@
 
 namespace PlayerUI
 {
-    class VideoObject
+    public class VideoObject
     {
         private string video_title, video_url, video_thumbnail;
         public VideoObject(string video_title, string video_url, string video_thumbnail)
